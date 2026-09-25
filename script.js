ce4a05f0061c22e6f8ff8d3c2223b1af8bf6c06c
code-with-anjaliiii <prajapatanjali700@gmail.com>
2026-09-25 14:26:11 +0530

    Add localStorage

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 let taskInput = document.getElementById("taskInput");
 let addButton = document.getElementById("addBtn");
 let list = document.getElementById("list");
+let tasks = [];
 
-
+let savedTasks = JSON.parse(localStorage.getItem("myTasks")) || [];
+savedTasks.forEach(function(task) {
+    let ele = document.createElement("li");
+        let del = document.createElement("button");
+        del.textContent = "Delete";
+        ele.textContent = task;
+        ele.className = "task";
+        list.appendChild(ele);
+        ele.appendChild(del);
+        del.addEventListener("click", function(){
+            ele.remove();
+            updateStorage();
+        })
+});
 addButton.addEventListener("click", function () {
     let task = taskInput.value;
     if (task === "") {
@@ -16,10 +30,20 @@ addButton.addEventListener("click", function () {
         ele.className = "task";
         list.appendChild(ele);
         ele.appendChild(del);
+        updateStorage();
         del.addEventListener("click", function(){
             ele.remove();
+            updateStorage();
         })
         taskInput.value = "";
         console.log("perfect!");
     }
 })
+function updateStorage(){
+    let allTasks = [];
+    let liItems = list.querySelectorAll("li");
+    liItems.forEach(function(li){
+        allTasks.push(li.firstChild.textContent);
+    });
+  localStorage.setItem("myTasks", JSON.stringify(allTasks));
+}
